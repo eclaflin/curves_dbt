388170fae71052f05600b256c1ef9889880b8c31
eclaflin <eric.claflin@gmail.com>
2024-07-24 21:57:19 -0400

    solve git requirement

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+apt-get update && apt-get install -y git
 pip install -r requirements.txt
 exec "$@"  # This allows overriding the command when running the container
 tail -f /dev/null  # Keep the container running
